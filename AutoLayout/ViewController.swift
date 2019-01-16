6f5d356840201c876d803eb585d33b3ecad1f143
Wee Hi <maweefeng@gmail.com>
2019-01-16 11:39:52 +0800

    first commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Alex wee on 2019/1/15.
+//  Copyright © 2019年 Alex wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+}
+
